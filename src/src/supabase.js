b423ee8b7ea781d85c890eebc877a04594e7cef8
davidsalom21 <davidsalom5@gmail.com>
2026-06-15 21:15:43 +0200

    Update supabase.js

--- src/src/supabase.js
+++ src/src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL      = "https://gnnqshlejvlweaomamcy.supabase.co";
-const SUPABASE_ANON_KEY = "PEGA_AQUI_TU_sb_publishable_...";
+const SUPABASE_ANON_KEY = "sb_publishable_amPlLaLPtyXkVkaGRanCpg_hCF4dw65";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
